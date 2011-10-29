1fc23fc7b1d51f06b074e96d9c631d8aecebed29
Rodrigo <rodrigo.fraga@gmail.com>
2011-10-29 15:25:15 -0400

    adding metadataserviceprovider

---
+++ AXTemplateGenerator.Aif.Service/IAxMetadataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AXTemplateGenerator.Aif.Service
+{
+    public interface IAxMetadataService: IServiceProvider
+    {
+        string[] GetTableNames();
+        string[] GetClassNames();
+        string[] GetExtendedDataTypeNames();
+        string[] GetEnumNames();
+        string[] GetQueryNames();
+        string[] GetServiceNames();
+        string[] GetFormPartNames();
+    }
+}
